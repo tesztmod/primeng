6e9283f842f42d47554840c13b511a0f85c5a1ba
Viktor Szvoreny <szvoreny@inf.u-szeged.hu>
2022-05-10 11:18:12 +0200

    knob, lightbox unit tests

--- src/app/components/knob/knob.spec.ts
+++ src/app/components/knob/knob.spec.ts
@@ -1,6 +1,16 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { Knob, KnobModule } from './knob';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <p-knob></p-knob>
+    `
+})
+class TestKnobComponent {
+    valueColor: string = "green";
+}
 
 describe('Knob', () => {
 
@@ -10,14 +20,128 @@ describe('Knob', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [
-				NoopAnimationsModule
+				NoopAnimationsModule,
+                KnobModule
 			],
 			declarations: [
-				KnobModule
+                TestKnobComponent
 			]
 		});
 
 		fixture = TestBed.createComponent(Knob);
 		tag = fixture.componentInstance;
+
 	});
+
+    it('check initial values', () => {
+        fixture.detectChanges();
+        expect(tag.midX).toEqual(50);
+        expect(tag.minRadians).toEqual(4 * Math.PI / 3);
+        expect(tag.maxRadians).toEqual(-Math.PI / 3);
+
+    });
+
+    it('check call of update value', () => {
+
+        fixture.detectChanges();
+
+        const updateModelStateSpy = spyOn(tag, 'updateModel');
+        tag.updateValue(1,1);
+		expect(updateModelStateSpy).toHaveBeenCalled();
+
+    });
+
+    it('check updateModel', () => {
+
+        fixture.detectChanges();
+        tag.updateModel(-Math.PI / 4,-Math.PI / 4);
+        expect(tag.value).toEqual(95);
+
+    });
+
+    it('check updateModel angle less than start', () => {
+
+        fixture.detectChanges();
+        tag.updateModel(-Math.PI / 3,-Math.PI / 4);
+        expect(tag.value).toEqual(-20);
+
+    });
+
+    it('check onClick', () => {
+        tag.onClick(event);
+        fixture.detectChanges();
+        const onClickStateSpy = spyOn(tag, 'onClick');
+        tag.onClick(event);
+        expect(onClickStateSpy).toHaveBeenCalled();
+
+    });
+
+    it('check onMouseDown', () => {
+        tag.onMouseDown(event);
+        fixture.detectChanges();
+        const onMouseDownStateSpy = spyOn(tag, 'onMouseDown');
+        tag.onMouseDown(event);
+        expect(onMouseDownStateSpy).toHaveBeenCalled();
+
+    });
+
+    it('check onMouseUp', () => {
+        tag.onMouseUp(event);
+        fixture.detectChanges();
+        const onMouseUpStateSpy = spyOn(tag, 'onMouseUp');
+        tag.onMouseUp(event);
+        expect(onMouseUpStateSpy).toHaveBeenCalled();
+    });
+
+    it('check writeValue', () => {
+
+        fixture.detectChanges();
+        tag.writeValue(1);
+        expect(tag.value).toEqual(1);
+
+    });
+
+    it('check registerOnChange', () => {
+        fixture.detectChanges();
+        tag.registerOnChange(Function);
+        expect(tag.onModelChange).toEqual(Function);
+
+    });
+
+    it('check registerOnTouched', () => {
+        fixture.detectChanges();
+        tag.registerOnTouched(Function);
+        expect(tag.onModelTouched).toEqual(Function);
+
+    });
+
+    it('check setDisabledState', () => {
+        fixture.detectChanges();
+        tag.setDisabledState(true);
+        expect(tag.disabled).toEqual(true);
+
+    });
+
+    it('check zeroRadians this max 0', () => {
+
+        fixture.detectChanges();
+        tag.max = 1;
+        tag.min = 1;
+
+        expect(tag.zeroRadians()).toEqual(NaN);
+
+    });
+
+    it('check zeroRadians this max 0', () => {
+
+        fixture.detectChanges();
+        spyOn(tag, 'largeArc').and.callFake(function(){
+            return 1;
+        });
+
+
+        expect(tag.largeArc()).toEqual(1);
+
+    });
+
 });
--- src/app/components/lightbox/lightbox.spec.ts
+++ src/app/components/lightbox/lightbox.spec.ts
@@ -1,17 +1,88 @@
 import { TestBed, ComponentFixture, tick, fakeAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { Lightbox } from './lightbox';
+import { Lightbox, LightboxModule } from './lightbox';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <p-lightbox></p-lightbox>
+    `
+})
+class TestLightboxComponent {
+}
 
 describe('Lightbox', () => {
+
+    let tag: Lightbox;
+	let fixture: ComponentFixture<Lightbox>;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
                 NoopAnimationsModule,
+                LightboxModule
             ],
             declarations: [
-                Lightbox
+                TestLightboxComponent
             ]
         });
+
+        fixture = TestBed.createComponent(Lightbox);
+		tag = fixture.componentInstance;
+
     });
+
+    it('check initial values', () => {
+
+        fixture.detectChanges();
+        tag.onLinkClick(event, null);
+        expect(tag).toBeTruthy();
+
+    });
+
+    it('check hide values', () => {
+        fixture.detectChanges();
+        tag.hide(event);
+        expect(tag.visible).toEqual(false);
+    });
+
+
+    it('check display image', () => {
+        const img = {title: "asd"};
+
+        fixture.detectChanges();
+        tag.displayImage(img);
+        expect(tag).toBeTruthy();
+    });
+
+    it('check image click', () => {
+        const img = {title: "asd"};
+        const cnt = {style: {width: "", height: ""}};
+        fixture.detectChanges();
+        tag.onImageClick(event, img, 1, cnt);
+        expect(tag.preventDocumentClickListener).toEqual(true);
+    });
+
+    it('check prev', () => {
+        const ph = {style: {display: ""}};
+        tag.images = ['1', '2'];
+        tag.index = 1;
+        fixture.detectChanges();
+        tag.prev(ph)
+        expect(tag.loading).toEqual(true);
+    });
+
+    it('check next', () => {
+        const ph = {style: {display: ""}};
+        tag.images = ['1', '2'];
+        tag.index = 0;
+        fixture.detectChanges();
+        tag.next(ph);
+        expect(tag.loading).toEqual(true);
+    });
+
+
+
+
 });
