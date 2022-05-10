acf9fffa0b0e413b91493323c03187d1cd534ab6
Szalaji Natália <sznati98@gmail.com>
2022-05-10 08:02:13 +0200

    Unit tests

--- src/app/components/carousel/carousel.spec.ts
+++ src/app/components/carousel/carousel.spec.ts
@@ -1,58 +1,63 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { Carousel } from './carousel';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { Carousel } from "./carousel";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-describe('Carousel', () => {
-  
+describe("Carousel", () => {
     let carousel: Carousel;
     let fixture: ComponentFixture<Carousel>;
 
     beforeEach(() => {
-    TestBed.configureTestingModule({
-        imports: [
-        NoopAnimationsModule
-        ],
-        declarations: [
-        Carousel
-        ]
+        TestBed.configureTestingModule({
+            imports: [NoopAnimationsModule],
+            declarations: [Carousel],
+        });
+
+        fixture = TestBed.createComponent(Carousel);
+        carousel = fixture.componentInstance;
+        carousel.value = [
+            { vin: "r3278r2", year: 2010, brand: "Audi", color: "Black" },
+            { vin: "jhto2g2", year: 2015, brand: "BMW", color: "White" },
+            { vin: "h453w54", year: 2012, brand: "Honda", color: "Blue" },
+            { vin: "g43gwwg", year: 1998, brand: "Renault", color: "White" },
+            { vin: "gf45wg5", year: 2011, brand: "VW", color: "Red" },
+            { vin: "bhv5y5w", year: 2015, brand: "Jaguar", color: "Blue" },
+            { vin: "ybw5fsd", year: 2012, brand: "Ford", color: "Yellow" },
+            { vin: "45665e5", year: 2011, brand: "Mercedes", color: "Brown" },
+            { vin: "he6sb5v", year: 2015, brand: "Ford", color: "Black" },
+        ];
     });
 
-    fixture = TestBed.createComponent(Carousel);
-    carousel = fixture.componentInstance;
-    carousel.value =[
-        {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
-        {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
-        {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
-        {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
-        {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
-        {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
-        {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
-        {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
-        {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
-    ];
-});
-
-    it('should created by default', () => {
+    it("should created by default", () => {
         fixture.detectChanges();
-  
-        const containerEl = fixture.debugElement.query(By.css('.p-carousel'));
+
+        const containerEl = fixture.debugElement.query(By.css(".p-carousel"));
         expect(containerEl.nativeElement).toBeTruthy();
     });
 
-    it('should create 9 elements', () => {
+    it("should create 9 elements", () => {
         fixture.detectChanges();
-  
-        const containerEl = fixture.debugElement.queryAll(By.css('.p-carousel-item'));
+
+        const containerEl = fixture.debugElement.queryAll(
+            By.css(".p-carousel-item")
+        );
         expect(containerEl.length).toEqual(9);
     });
 
-    it('should call the navForward (circular)', () => {
+    it("should set page", () => {
+        carousel.isCreated = true;
+        carousel._page = 1;
+        carousel.autoplayInterval = 100;
+        carousel.page = 2;
+        expect(carousel.allowAutoplay).toBe(false);
+    });
+
+    it("should call the navForward (circular)", () => {
         carousel.circular = true;
         fixture.detectChanges();
-  
-        const onNextNavSpy = spyOn(carousel,"navForward").and.callThrough();
-        const nextEl = fixture.debugElement.query(By.css('.p-carousel-next'));
+
+        const onNextNavSpy = spyOn(carousel, "navForward").and.callThrough();
+        const nextEl = fixture.debugElement.query(By.css(".p-carousel-next"));
         expect(carousel.page).toEqual(0);
         nextEl.nativeElement.click();
         fixture.detectChanges();
@@ -66,12 +71,12 @@ describe('Carousel', () => {
         expect(carousel.page).toEqual(3);
     });
 
-    it('should call the navBackward (circular)', () => {
+    it("should call the navBackward (circular)", () => {
         carousel.circular = true;
         fixture.detectChanges();
-  
-        const onPrevNavSpy = spyOn(carousel,"navBackward").and.callThrough();
-        const prevEl = fixture.debugElement.query(By.css('.p-carousel-prev'));
+
+        const onPrevNavSpy = spyOn(carousel, "navBackward").and.callThrough();
+        const prevEl = fixture.debugElement.query(By.css(".p-carousel-prev"));
         expect(carousel.page).toEqual(0);
         prevEl.nativeElement.click();
         fixture.detectChanges();
@@ -84,7 +89,7 @@ describe('Carousel', () => {
         expect(carousel.page).toEqual(7);
     });
 
-    it('should call updateState when window resize', () => {
+    it("should call updateState when window resize", () => {
         fixture.detectChanges();
 
         expect(carousel.totalDots()).toEqual(9);
@@ -103,16 +108,16 @@ describe('Carousel', () => {
         expect(carousel.totalDots()).toEqual(9);
     });
 
-    it('should show with autoPlay', (done) => {
+    it("should show with autoPlay", (done) => {
         carousel.autoplayInterval = 100;
         fixture.detectChanges();
 
         expect(carousel.page).toEqual(0);
         fixture.detectChanges();
 
-        setTimeout(()=> {
+        setTimeout(() => {
             expect(carousel.page).toEqual(1);
             done();
-        },102);
+        }, 102);
     });
 });
--- src/app/components/inputnumber/inputnumber.spec.ts
+++ src/app/components/inputnumber/inputnumber.spec.ts
@@ -1,34 +1,30 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { InputNumber } from './inputnumber';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { Component } from '@angular/core';
-import { By } from '@angular/platform-browser';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { InputNumber } from "./inputnumber";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { Component } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 @Component({
-    template: `<p-inputNumber [(ngModel)]="val" [readonly]="readonly"></p-inputNumber>`
+    template: `<p-inputNumber
+        [(ngModel)]="val"
+        [readonly]="readonly"
+    ></p-inputNumber>`,
 })
 class TestInputNumberComponent {
     val: number;
     readonly: boolean = true;
 }
 
-describe('InputNumber', () => {
-
+describe("InputNumber", () => {
     let inputNumber: InputNumber;
     let testComponent: TestInputNumberComponent;
     let fixture: ComponentFixture<TestInputNumberComponent>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [
-                NoopAnimationsModule,
-                FormsModule
-            ],
-            declarations: [
-                InputNumber,
-                TestInputNumberComponent
-            ]
+            imports: [NoopAnimationsModule, FormsModule],
+            declarations: [InputNumber, TestInputNumberComponent],
         });
 
         fixture = TestBed.createComponent(TestInputNumberComponent);
@@ -36,10 +32,246 @@ describe('InputNumber', () => {
         testComponent = fixture.componentInstance;
     });
 
-    it('should display by default', () => {
+    it("should display by default", () => {
         fixture.detectChanges();
 
-        const inputMaskEl = fixture.debugElement.query(By.css('input'));
+        const inputMaskEl = fixture.debugElement.query(By.css("input"));
         expect(inputMaskEl.nativeElement).toBeTruthy();
+    });
+
+    it("should disable", () => {
+        inputNumber.disabled = true;
+        expect(inputNumber.focused).toBe(false);
+    });
+
+    it("should return the currency expression", () => {
+        inputNumber.currency = "EUR";
+        const ret = inputNumber.getCurrencyExpression();
+
+        expect(ret).not.toBeNull();
+    });
+
+    describe("formatValue()", () => {
+        it("should return '-'", () => {
+            const ret = inputNumber.formatValue("-");
+            expect(ret).toEqual("-");
+        });
+    });
+
+    describe("onUpButtonKeyDown", () => {
+        const keyEvent = new KeyboardEvent("keydown", { keyCode: 32 });
+
+        it("should call repeat", () => {
+            spyOn(inputNumber, "repeat");
+            inputNumber.onUpButtonKeyDown(keyEvent);
+        });
+    });
+
+    describe("formatValue()", () => {
+        it("should return '-'", () => {
+            const ret = inputNumber.formatValue("-");
+            expect(ret).toEqual("-");
+        });
+    });
+
+    it("should return add prefix to formatted value", () => {
+        inputNumber.prefix = "macska";
+        const ret = inputNumber.formatValue("asdada-");
+
+        expect(ret).toEqual("macskaNaN");
+    });
+
+    it("should return add suffix to formatted value", () => {
+        inputNumber.suffix = "macska";
+        const ret = inputNumber.formatValue("asdada-");
+
+        expect(ret).toEqual("NaNmacska");
+    });
+
+    it("should return the input value", () => {
+        inputNumber.format = false;
+        const ret = inputNumber.formatValue("asdada-");
+        expect(ret).toEqual("asdada-");
+    });
+
+    describe("getPrefixExpression", () => {
+        it("should set the prefixChar value", () => {
+            inputNumber.prefix = "asd";
+            inputNumber.getPrefixExpression();
+            expect(inputNumber.prefixChar).toEqual(inputNumber.prefix);
+        });
+    });
+
+    describe("getSuffixExpression", () => {
+        it("should set the prefixChar value", () => {
+            inputNumber.suffix = "asd";
+            inputNumber.getSuffixExpression();
+            expect(inputNumber.suffixChar).toEqual(inputNumber.suffix);
+        });
+    });
+
+    describe("parseValue()", () => {
+        it("should return null if the filtered text is not a - sign", () => {
+            const ret = inputNumber.parseValue("asdasd");
+            console.log(ret);
+        });
+    });
+
+    describe("parseValue()", () => {
+        it("should return null if the filtered text is not a - sign", () => {
+            const ret = inputNumber.parseValue("asdasd");
+            expect(ret).toBeNull();
+        });
+
+        it("should return null if the filtered text is not a - sign", () => {
+            const ret = inputNumber.parseValue("asdasd");
+            expect(ret).toBeNull();
+        });
+
+        it("should return a - sign", () => {
+            const ret = inputNumber.parseValue("-");
+            expect(ret).toEqual("-");
+        });
+    });
+
+    describe("allowMinusSign", () => {
+        it("should return true, when min value is undefined", () => {
+            const ret = inputNumber.allowMinusSign();
+            expect(ret).toBe(true);
+        });
+        it("should return true, when min is negative", () => {
+            inputNumber.min = -1;
+            const ret = inputNumber.allowMinusSign();
+            expect(ret).toBe(true);
+        });
+        it("should return false, when min is positive", () => {
+            inputNumber.min = 1;
+            const ret = inputNumber.allowMinusSign();
+            expect(ret).toBe(false);
+        });
+    });
+
+    describe("isDecimalMode", () => {
+        it("should be in decimal mode", () => {
+            inputNumber.mode = "decimal";
+            const ret = inputNumber.isDecimalMode();
+            expect(ret).toBe(true);
+        });
+    });
+
+    describe("isValueChanged", () => {
+        it("should return true if the value is changed", () => {
+            const ret = inputNumber.isValueChanged("1", "2");
+            expect(ret).toBe(true);
+        });
+
+        it("should return true if the new value null", () => {
+            const ret = inputNumber.isValueChanged("1", null);
+            expect(ret).toBe(true);
+        });
+
+        it("should return false if the new value is null and the curent value is nul", () => {
+            const ret = inputNumber.isValueChanged(null, null);
+            expect(ret).toBe(false);
+        });
+
+        it("should return true if th value are not changed", () => {
+            const ret = inputNumber.isValueChanged("1", "1");
+            expect(ret).toBe(true);
+        });
+    });
+
+    describe("validateValue", () => {
+        it("should return null if the value i - sign", () => {
+            const ret = inputNumber.validateValue("-");
+            expect(ret).toBeNull();
+        });
+        it("should return null if the value i - sign", () => {
+            const ret = inputNumber.validateValue("-");
+            expect(ret).toBeNull();
+        });
+        it("should return the min attribute if it is bigger than the value", () => {
+            inputNumber.min = 2;
+            const ret = inputNumber.validateValue(1);
+            expect(ret).toBe(2);
+        });
+        it("should return the max attribute if it is smaller than the value", () => {
+            inputNumber.max = 2;
+            const ret = inputNumber.validateValue(4);
+            expect(ret).toBe(2);
+        });
+        it("should return the value if it is smaller than min the value", () => {
+            inputNumber.max = 2;
+            const ret = inputNumber.validateValue(1);
+            expect(ret).toBe(1);
+        });
+    });
+    describe("onUpButtonKeyUp", () => {
+        it("should call clear Timer", () => {
+            spyOn(inputNumber, "clearTimer");
+            inputNumber.onUpButtonKeyUp();
+            expect(inputNumber.clearTimer).toHaveBeenCalled();
+        });
+    });
+    describe("onDownButtonMouseUp", () => {
+        it("should call clear Timer", () => {
+            spyOn(inputNumber, "clearTimer");
+            inputNumber.onDownButtonMouseUp();
+            expect(inputNumber.clearTimer).toHaveBeenCalled();
+        });
+    });
+    describe("onDownButtonMouseLeave", () => {
+        it("should call clear Timer", () => {
+            spyOn(inputNumber, "clearTimer");
+            inputNumber.onDownButtonMouseLeave();
+            expect(inputNumber.clearTimer).toHaveBeenCalled();
+        });
+    });
+    describe("onDownButtonKeyUp", () => {
+        it("should call clear Timer", () => {
+            spyOn(inputNumber, "clearTimer");
+            inputNumber.onDownButtonKeyUp();
+            expect(inputNumber.clearTimer).toHaveBeenCalled();
+        });
+    });
+    describe("onUserInput", () => {
+        let keyEvent = new KeyboardEvent("keydown", { key: "a" });
+
+        it("should return if the input is readonly", () => {
+            inputNumber.readonly = true;
+            const ret = inputNumber.onUserInput(keyEvent);
+            expect(ret).toBeUndefined;
+        });
+        it("should set isSpecialChar to false", () => {
+            inputNumber.readonly = false;
+            inputNumber.lastValue = "a";
+            const ret = inputNumber.onUserInput(keyEvent);
+            expect(inputNumber.isSpecialChar).toBe(false);
+        });
+    });
+    describe("onInputKeyDown", () => {
+        let mockEvemt = {
+            target: { value: "asd", selectionStart: 1, selectionEnd: 1 },
+            shiftKey: true,
+        };
+
+        it("should return if the input is readonly", () => {
+            inputNumber.readonly = true;
+            const ret = inputNumber.onInputKeyDown(mockEvemt);
+            expect(ret).toBeUndefined;
+        });
+    });
+
+    describe("deleteRange", () => {
+        it("should return empty string", () => {
+            const value = { length: 3, slice: (tmp = 0, tmp2 = 0) => "asd" };
+            const ret = inputNumber.deleteRange(value, 1, 2);
+            expect(ret).toEqual("asdasd");
+        });
+        it("should return empty string", () => {
+            const value = { length: 1, slice: (tmp = 0, tmp2 = 0) => "asd" };
+            const ret = inputNumber.deleteRange(value, 0, 2);
+            expect(ret).toEqual("asd");
+        });
     });
 });
